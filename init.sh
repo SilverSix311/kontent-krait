436502bf636f2a3ab91e5c2e9e5cbceef8eeb319
SilverSix311 <silversix311@gmail.com>
2022-02-14 19:23:51 +0000

    init.sh changes

--- init.sh
+++ init.sh
@@ -3,7 +3,10 @@
 apt install git ansible -y
 
 adduser krait
+#usermod -aG sudo krait
+#mkdir -p /tmp/kontent-krait/git
 
-git clone https://github.com/SilverSix311/kontent-krait.git
-chown -R krait:krait
-cd /opt/kontent-krait
+git clone https://github.com/SilverSix311/kontent-krait.git /tmp/kontent-krait/git/kontent-krait
+
+sudo mv /tmp/kontent-krait/git/kontent-krait /opt/
+sudo chown -R krait:krait /opt/kontent-krait
