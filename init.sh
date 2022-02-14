2cbb58a06d1aff299338706758ae85ad182e2c42
SilverSix311 <silversix311@gmail.com>
2022-02-14 21:03:19 +0000

    Removed Sudo's from init.sh

--- init.sh
+++ init.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-sudo apt install curl git ansible -y
+apt install curl git ansible -y
 
 adduser krait
 #usermod -aG sudo krait
-#mkdir -p /tmp/kontent-krait/git
 
 git clone https://github.com/SilverSix311/kontent-krait.git /tmp/kontent-krait/git/kontent-krait
 
-sudo mv /tmp/kontent-krait/git/kontent-krait /opt/
-sudo chown -R krait:krait /opt/kontent-krait
+mv /tmp/kontent-krait/git/kontent-krait /opt/
+chown -R krait:krait /opt/kontent-krait
 
 #ansible-playbook /opt/kontent-krait/playbooks/sudoers.yaml -i /opt/kontent-krait/inventory/hosts --extra-vars "ansible_sudo_pass=$sudopass"
